89f50e327b941b1810a1320a5db7bd4972aa2772
suzaku183 <suzaku622@gmail.com>
2017-11-08 20:29:19 +0900

    パターンマッチ難しい

--- erl/parser.erl
+++ erl/parser.erl
@@ -1,9 +1,10 @@
 -module(parser).
--export([execute/1, read_from/1, while/1]).
+-export([execute/1, read_from/1, while/1, to_atom/1]).
 
 execute(Token) ->
 		Tokens = compact(Token),
-		read_from(Tokens).
+		Tokens.
+		%% read_from(Tokens).
 
 tokenize(Token)->
 		S = re:replace(Token, "\\(", " ( ", [global, {return, list}]),
@@ -14,25 +15,41 @@ compact(Token) ->
 		T = tokenize(Token),
 		[L || L <- T, length(L) >= 1].
 
+to_atom(Value) ->
+		{_, CondFloat} = re:compile("[0-9]+\.[0-9]+"),
+		RegFloat = re:run(Value, CondFloat),
+		case RegFloat of
+				{match, _} ->
+						hd(string:to_float(Value))
+		end.
 
-read_from(Tokens) ->
-		[Car|Cdr] = Tokens,
-		read_from(Car, Cdr).
+type_translate(Token) ->
+		[to_atom(Value) || Value <- Token].
 
-read_from("(", Cdr) ->
-		while(Cdr);
-read_from(")", Cdr) ->
-		read_from(Cdr);
-read_from(Car, _) ->
-		[Car].
+read_from(X) ->
+		type_translate(X).
 
-while(List) ->
-		[H|T] = List,
-		L = [],
-		while(H,T,L).
+while(X) ->
+		X.
 
-while(H, _, L) when H == ")" ->
-		L;
-while(H, T, L) ->
-		[Car|Cdr] = T,
-		while(Car, Cdr, lists:append(L, [H])).
+%% read_from(Tokens) ->
+%%  		[Car|Cdr] = Tokens,
+%%  		read_from(Car, Cdr).
+%%  
+%% read_from("(", Cdr) ->
+%%  		while(Cdr);
+%% read_from(")", Cdr) ->
+%%  		read_from(Cdr);
+%% read_from(Car, _) ->
+%%  		[Car].
+%%  
+%% while(List) ->
+%%  		[H|T] = List,
+%%  		L = [],
+%%  		while(H,T,L).
+%%  
+%% while(H, _, L) when H == ")" ->
+%%  		L;
+%% while(H, T, L) ->
+%%  		[Car|Cdr] = T,
+%%  		while(Car, Cdr, lists:append(L, [H])).
--- erl/parser_test.erl
+++ erl/parser_test.erl
@@ -1,6 +1,14 @@
 -module(parser_test).
 -include_lib("eunit/include/eunit.hrl").
+-export([test/0]).
 
-tokenize_test() ->
-		["+", 1, 2] == parser:tokenize("(+ 1 2)").
-
+test() ->
+		execute_is_return_tokenized_code().
+		
+execute_is_return_tokenized_code() ->
+		% ?assertEqual(["+", 1, 2], parser:execute("(+ 1 2)")),
+		% ?assertEqual("+", parser:to_atom("+")),
+		% ?assertEqual(1, parser:to_atom("1")),
+		?assertEqual(1.1, parser:to_atom("1.1")).
+    %% ?assertEqual(["+", "1", "2"], parser:execute("(+ 1 2 (- 1 2))")),
+		%% ?assertEqual(["+", "1", "2"], parser:execute("(+ 1 2 (* 1 2 (/ 10 20)) 10 20)")).		
